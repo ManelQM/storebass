96bd45880ef1254df8b70225fe219cbd04ced368
Manel <quemonmanel@gmail.com>
2023-05-29 13:14:00 +0200

    feat: routes + auth + views

---
+++ router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router(); 
+
+const auth = require ("./config/auth");
+
+const UserRouter = require("./views/UserRouter");
+const AuthRouter = require("./views/AuthRouter"); 
+
+// router.use("/user", UserRouter); // Register and Login routes
+// router.use("/auth", AuthRouter); 
+
+module.exports = router; 
