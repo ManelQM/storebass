d08e206fe31c68b79b3e04335c5ce09714351555
Manel <quemonmanel@gmail.com>
2023-05-30 22:12:55 +0200

    feat: user controller

--- views/UserRouter.js
+++ views/UserRouter.js
@@ -1,4 +1,8 @@
 const express = require("express"); 
-const router = express.Router; 
+const router = express.Router();
 
 const userController = require("../controllers/userController"); 
+
+// router.get("/user/all", userController.getAll);
+
+module.exports = router
