f3fb40763a674edfca7d9f49cf08044dddaf12ff
Manel <quemonmanel@gmail.com>
2023-08-04 20:16:45 +0200

    feat: new login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,74 +1,10 @@
-// const {models,user} = require("../models/index");
+const {models} = require("../models/index");
 // const {user} = require("../models/user");
 // const { Sequelize, Op } = require("sequelize");
 // const bcrypt = require("bcrypt");
 // const jwt = require("jsonwebtoken"); 
 // const authConfig = require("../config/auth");
 
-const {b}
+const {bcryptCompare} = require("../services/authServices"); 
+require("dotenv").config();
 
-
-const AuthController = {}; 
-
-// LOGIN 
-AuthController.login = (req, res) => {
-    let { email, password } = req.body; 
-   console.log("arrrgggggggggg", user)
-    //Search User  
-   models.user.findOne({where: {email: email }
-    }).then(user => {
-        if (!user) {
-            res.status(404).json({msg : "User not found"})
-        } else {
-            // TALES FROM THE ENCRYPT
-            if (bcrypt.compareSync(password, user.password)) {
-                // THE CREATION OF THE TOKEN
-                let token = jwt.sign({ user: user }, authConfig.secret, {
-                    expiresIn: authConfig.expires
-                });
-                res.json({
-                    user: user,
-                    token: token
-                })
-            } else {
-                res.status (401).json ({ msg: "Invalid Password"})
-            }
-        }
-    }).catch (err => {
-        res.status(500).json(err);
-    })
-};
-
-//REGISTER 
-
-AuthController.register = (req, res)=> {
-
-    let password = bcrypt.hashSync(req.body.password, Number.parseInt(authConfig.rounds));
-
-    //CREAR USUARIO
-
-    user.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: password
-
-    }).then(user => {
-
-        //THE CREATION OF THE TOKEN
-        let token = jwt.sign({ user: user }, authConfig.secret, {
-            expiresIn: authConfig.expires
-        });
-        
-        res.json({
-            user: user,
-            token: token
-        });
-
-    }).catch(err => {
-        res.status(500).json(err);
-
-    });
-};
-
-
-module.exports = AuthController
