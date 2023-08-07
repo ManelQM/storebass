7e639bab099aafbb0112f36bd473b7d296e8f027
Manel <quemonmanel@gmail.com>
2023-08-07 23:27:26 +0200

    feat: login controller made+

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,6 +10,30 @@ const authLoginController = async (req,res) => {
   
   try {
     const result = await models.User.findOne({where:{email: email}}); 
+    if (!result) {
+      res.status(401).json({message: "Email or password doesnt match"});
+      return;
+    }
+    if(result.deleted == true) {
+      res.status(401).json({message:"Access Denied: You Shall Not Pass!!"});
+      return;
+    }
+    const responseBcryptCompare = await bcryptCompare(password, result.password);
+    if(responseBcryptCompare == false) {
+      res.status(400).json({message: "Password or Email are incorrect"});
+    }else {
+      const secret = process.env.ACCESS_TOKEN_SECRET;
+      
+      let token = jsonwebtoken.sign({ user: result}, authConfig.secret,{
+        expiresIn:authConfig.expires,
+      });
+      res.status(200).json({
+        message: "Login with success",
+        jwt: token,
+      })
+    }
+  }catch (error) {
+    res.send(error);
   }
 }
   
