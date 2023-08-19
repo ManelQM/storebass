a174e1d770fa5f30aa076648485ced16bf250d99
Manel <quemonmanel@gmail.com>
2023-08-19 20:17:51 +0200

    feat:authbearermiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,2 +1,32 @@
 const jsonwebtoken = require("jsonwebtoken");
 require("dotenv").config();
+
+// AUTHENTIFICATION 
+
+const autheBearerMiddleware = async (req,res,next) => {
+    const {authorization} = req.headers;
+    try{
+        const [strategy, jwt] = authorization.split(" ");
+        if (strategy.toLowerCase() !== "bearer") {
+            throw new Error ("Invalid strategy");
+        }
+
+        const payload = jsonwebtoken.verify(jwt, process.env,JWT_SECRET);
+        req.auth = payload; 
+        next();
+    } catch (error) {
+        res
+        .status(401)
+        .json({message: "Not authentification. Please log"}); 
+        return;
+    }
+};
+
+
+
+
+
+
+module.exports = {
+    autheBearerMiddleware,
+}
