045d8410c0a3cc22520893ac77870d856973ae79
Manel <quemonmanel@gmail.com>
2023-08-20 12:26:17 +0200

    chore:admin routes,view + controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const {User} = require("../models/index");
 const {encryptPasswordService} = require ("../services/authServices");  
 require("dotenv").config();
 
-
+// const bringAllUsers = async (req,res) => 
 
 
  
--- router.js
+++ router.js
@@ -4,8 +4,10 @@ const auth = require ("./config/auth");
 
 const UserRouter = require("./views/UserRouter");
 const AuthRouter = require("./views/AuthRouter"); 
+const AdminRouter = require("./views/AdminRouter");
 
 router.use("/user", UserRouter); 
 router.use("/auth", AuthRouter); 
+router.use("/admin",AdminRouter);
 
 module.exports = router; 
