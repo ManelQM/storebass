a5720ae8d90399c361e0f515c81f8d6a0cd77a62
Manel <quemonmanel@gmail.com>
2023-08-27 20:54:04 +0200

    feat:allstore controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 const models = require("../models/index");
 const { User } = models
+const {Productstore} = models
 require("dotenv").config();
 
 const bringAllUsers = async (req, res) => {
@@ -16,6 +17,19 @@ const bringAllUsers = async (req, res) => {
   }
 };
 
+
+const bringAllStore = async (req, res) => {
+  try {
+    const store = await Productstore.findAll({
+    });
+    res.json({message: "Store list of products", store});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error : "Server Error"});
+  }
+};
+
 module.exports = {
   bringAllUsers,
+  bringAllStore,
 };
--- views/AdminRouter.js
+++ views/AdminRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { autheBearerMiddleware ,isValidRole, isValidUser } = require("../middlewares/authMiddleware");
 const {
-    bringAllUsers,
+    bringAllUsers,bringAllStore
 } = require("../controllers/adminController");
 
 
 router.get("/allusers",autheBearerMiddleware,isValidRole(1),bringAllUsers);
+router.get("/allstore",autheBearerMiddleware,isValidRole(1),bringAllStore)
 
 module.exports = router 
