55357c226c3bde7847efb6c31f7df8187e6a9a1d
Manel <quemonmanel@gmail.com>
2023-08-29 18:10:30 +0200

    feat: delete users by admin controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,6 @@
 const models = require("../models/index");
-const { User } = models
-const {Productstore} = models
+const { User } = models;
+const { Productstore } = models;
 require("dotenv").config();
 
 const bringAllUsers = async (req, res) => {
@@ -17,19 +17,44 @@ const bringAllUsers = async (req, res) => {
   }
 };
 
-
 const bringAllStore = async (req, res) => {
   try {
-    const pstore = await Productstore.findAll({
-    });
-    res.json({message: "Store list of products", pstore});
+    const pstore = await Productstore.findAll({});
+    res.json({ message: "Store list of products", pstore });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error : "Server Error"});
+    return res.status(500).json({ error: "Server Error" });
   }
 };
 
+const deleteUser = async (req,res) => {
+  try{
+    const email = req.body.email;
+    const deleteThisUser = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (deleteThisUser.roleid === 1) {
+      res.json ({
+        message: "Untouchable Admin"
+      });
+    } else {
+      User.destroy({
+        where: {
+          email: email,
+        },
+      });
+      res,json({
+        message: "User deleted from the database"
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 module.exports = {
   bringAllUsers,
   bringAllStore,
+  deleteUser,
 };
--- views/AdminRouter.js
+++ views/AdminRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { autheBearerMiddleware ,isValidRole, isValidUser } = require("../middlewares/authMiddleware");
 const {
     bringAllUsers,
-    bringAllStore
+    bringAllStore,
+    deleteUser,
 } = require("../controllers/adminController");
 
 
 router.get("/allusers",autheBearerMiddleware,isValidRole(1),bringAllUsers);
-router.get("/allstore",autheBearerMiddleware,isValidRole(1),bringAllStore)
+router.get("/allstore",autheBearerMiddleware,isValidRole(1),bringAllStore);
+router.delete("/deleteuser",autheBearerMiddleware,isValidRole(1),deleteUser);
 
 module.exports = router 
