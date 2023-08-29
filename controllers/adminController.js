a1ed9b12dcdd8fd6c0d954329e885e0dba24d93c
Manel <quemonmanel@gmail.com>
2023-08-29 22:43:27 +0200

    feat: admin deleteProduct controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -49,8 +49,30 @@ const deleteUser = async (req, res) => {
     console.error(error);
   }
 };
+
+const deleteProduct = async (req,res) => {
+  try {
+    const productName = req.body.name; 
+    const deleteThisProduct = await Productstore.findOne ({
+      where: {
+        name : productName,
+      },
+    }); 
+    if (!deleteThisProduct) {
+      return res.status(404).json({message: "Product not found in the store"});
+    } else {
+      await deleteThisProduct.destroy();
+      res.status(200).json({ message: "Product deleted with success"});
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Server Error"})
+  }
+};
+
 module.exports = {
   bringAllUsers,
   bringAllStore,
   deleteUser,
+  deleteProduct
 };
--- views/AdminRouter.js
+++ views/AdminRouter.js
@@ -5,11 +5,13 @@ const {
     bringAllUsers,
     bringAllStore,
     deleteUser,
+    deleteProduct,
 } = require("../controllers/adminController");
 
 
 router.get("/allusers",autheBearerMiddleware,isValidRole(1),bringAllUsers);
 router.get("/allstore",autheBearerMiddleware,isValidRole(1),bringAllStore);
 router.delete("/deleteuser",autheBearerMiddleware,isValidRole(1),deleteUser);
+router.delete("/deleteproduct", autheBearerMiddleware,isValidRole(1),deleteProduct)
 
 module.exports = router 
