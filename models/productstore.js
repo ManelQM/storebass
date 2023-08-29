098e13cb5c76b9f012b85031990758242a0a1653
Manel <quemonmanel@gmail.com>
2023-08-29 11:05:11 +0200

    chore:new columns

--- models/productstore.js
+++ models/productstore.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Productstore extends Model {
     /**
@@ -12,54 +10,60 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Productstore.hasMany(models.Orders, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Amps, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Books, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Cabinets, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Cables, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Cases, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Newbasses, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Pedals, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Picks, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Pickups, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Straps, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Strings, {
         foreignKey: "productstoreid",
-       });
+      });
       Productstore.hasOne(models.Vintagebasses, {
         foreignKey: "productstoreid",
-       });
+      });
     }
   }
-  Productstore.init({
- 
-    name: {
-      type:DataTypes.STRING,
-      allowNull: false 
+  Productstore.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  }, {
-    sequelize,
-    modelName: 'Productstore',
-  });
+    {
+      sequelize,
+      modelName: "Productstore",
+    }
+  );
   return Productstore;
 };
