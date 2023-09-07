3ded72c61bceb3bb97419b203b8f62224d2f7af5
Manel <quemonmanel@gmail.com>
2023-09-07 12:25:31 +0200

    chore:fixed problems with userid at addtocart controller but know new problems with stockproducts quantity

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,46 +1,43 @@
 const models = require("../models/index");
+const { Cart, Cartproduct, Productstore, User } = models;
 
-const {Cart ,Cartproduct, Productstore,User } = models; 
+const addProductToCart = async (req, res) => {
+//   console.log(req, "la req");
+  try {
+    const { productstoreid, quantity } = req.body;
+    const userid = req.auth.user.id;
 
+    // console.log(req.auth.user.id, "Que me traes¿?¿?¿?¿?");
+    let cart = await Cart.findOne({
+      where: {
+        userid,
+      },
+    });
 
-const addProductToCart = async (req,res) => {
-    console.log (req,"la req")
-    try{
-        const {productid, quantity} = req.body;
-        const userid = req.auth.id;
-        
-        console.log(req.auth.id, "Que me traes¿?¿?¿?¿?");
-        let cart = await Cart.findOne({
-            where: {
-                userid
-            },
-        });
-
-        if (!cart) {
-            cart = await Cart.create({userid});
-        }
-
-        const productstore = await Productstore.findByPk(productid);
-
-        if(!productstore || productstore.stock < quantity) {
-            return res.status(400).json({error: "This product is sold out"})
-        }
-        const cartproduct = await Cartproduct.create({
-            cartid: cart.id,
-            productid,
-            quantity,
-        });
-        
-        productstore.stock -= quantity;
-        await productstore.save()
-        res.status(201).json({message: `${Productstore.name},Added to Cart`});
-        
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({error: "Cant add product"});
+    if (!cart) {
+      cart = await Cart.create({ userid });
     }
+
+    const productstore = await Productstore.findByPk(productstoreid);
+
+    if (!productstore || productstore.stock < quantity) {
+      return res.status(400).json({ error: "This product is sold out" });
+    }
+    const cartproduct = await Cartproduct.create({
+      cartid: cart.id,
+      productstoreid,
+      quantity,
+    });
+
+    productstore.stock -= quantity;
+    await productstore.save();
+    res.status(201).json({ message: `${Productstore.name},Added to Cart` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Cant add product" });
+  }
 };
 
 module.exports = {
-    addProductToCart,
-}
+  addProductToCart,
+};
