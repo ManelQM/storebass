59fc6952d4679926bd09ceda73dc0114335f3589
Manel <quemonmanel@gmail.com>
2023-09-11 19:18:57 +0200

    chore:nightmare model dont insert into cartproducts

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -28,7 +28,10 @@ const addProductToCart = async (req, res) => {
       productstoreid,
       quantity,
     });
-
+    console.log(cartproduct, "cart")
+    // if (!cartproduct) {
+    //   return res.status(404).json({ error: "Cart not found" });
+    // }
     productstore.stock -= quantity;
     await productstore.save();
     res.status(201).json({ message: `${Productstore.name},Added to Cart` });
