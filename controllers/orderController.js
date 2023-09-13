d4556a1d24475a08cc228cd6687e1953f5950abe
Manel <quemonmanel@gmail.com>
2023-09-13 18:53:41 +0200

    feat: makeorder controller

---
+++ controllers/orderController.js
@@ -0,0 +1,46 @@
+const models = require("../models/index"); 
+const { Cart, Cartproduct, Productstore, User, Orders, Orderproduct} = models; 
+
+
+const makeOrder = async (req,res) => {
+    try{
+        const userid = req.auth.user.id; 
+
+        const cart = await Cart.findOne({
+            where : {
+                userid,
+            },
+            include: [Cartproduct],
+        }); 
+        if (!cart) {
+            return res.status(400).json({error: "Cart not found"})
+        }
+
+        const order = await Orders.create({
+            userid,
+            ships : req.body.ships,
+        });
+
+        const productsInCart = cart.Cartproducts;
+        for(const productInCart of productsInCart) {
+            await Orderproduct.create ({
+                orderid : order.id,
+                productstoreid : productInCart.productstoreid,
+                quantity : productInCart.quantity,
+            });
+            const productstore = await Productstore.findByPk(productInCart.productstoreid);
+            if (productstore) {
+                productstore.stock -= productInCart.quantity;
+                await productstore.save();
+            }
+        }
+        res.status(201).json({message: "Your order has been send to our headquarters. Thank your very much."})
+    } catch (error) {
+        console.error (error); 
+        res.status(500).json({error: "Cant make the order, please try again"});
+    }
+}; 
+
+module.exports = {
+    makeOrder,
+};
